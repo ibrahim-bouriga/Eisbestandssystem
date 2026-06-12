2f71f3a3e299d12a84c5b33e746645871d6f170f
Ibrahim Bouriga <ibrahim.bouriga@gmail.com>
2026-06-12 10:45:27 +0200

    Rename main class

--- src/main/java/com/kfru/App/EisbestandssystemApplication.java
+++ src/main/java/com/kfru/App/EisbestandssystemApplication.java
@@ -1,4 +1,4 @@
-package com.kfru.eisbestandssystem;
+package com.kfru.App;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
