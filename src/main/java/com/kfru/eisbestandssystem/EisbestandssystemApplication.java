42199bf0edba5ce9dcbeba4644e21c82b2b9a89c
Ibrahim-Bouriga <Ibrahim.Bouriga@ibm.com>
2026-06-12 10:26:46 +0200

    Initial init

---
+++ src/main/java/com/kfru/eisbestandssystem/EisbestandssystemApplication.java
@@ -0,0 +1,13 @@
+package com.kfru.eisbestandssystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EisbestandssystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EisbestandssystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kfru/eisbestandssystem/EisbestandssystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kfru.eisbestandssystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EisbestandssystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
